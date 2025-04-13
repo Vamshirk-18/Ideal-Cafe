5089132dd9a4e363b9cfd170f4420a39a178bf57
acharyaraksha84 <shrirakshapacharya@gmail.com>
2025-04-13 21:35:29 +0530

    All Done

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -7,7 +7,7 @@ function Profile() {
       <Card className="p-4 shadow">
         <h3 className="text-center mb-4">👤 User Profile</h3>
         <p><strong>Name:</strong> Narendra Modi</p>
-        <p><strong>Email:</strong> nmodi@example.com</p>
+        <p><strong>Email:</strong> nmodi@gmail.com</p>
         <p><strong>Favorite Icecream:</strong> Gadbad</p>
         <p><strong>Member Since:</strong> April 2024</p>
       </Card>
